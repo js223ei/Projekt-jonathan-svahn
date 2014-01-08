73f9cc97cad1933ef0038c6d0e63b6fa8442981d
js223ei <js223ei@student.lnu.se>
2014-01-08 16:57:02 +0100

    4

---
+++ javascript/uggla4.js
@@ -0,0 +1,15 @@
+ 	alert:("sdfs");
+	console.log("hjej");
+	
+	
+	var bilder = ['../../css/pics/uggla2.PNG', '../../css/pics/uggla3.PNG', '../../css/pics/uggla1.PNG', '../../css/pics/uggla1.PNG', '../../css/pics/uggla1.PNG', '../../css/pics/uggla1.PNG', '../../css/pics/uggla1.PNG', '../../css/pics/uggla1.PNG', '../../css/pics/uggla1.PNG', '../../css/pics/uggla1.PNG', '../../css/pics/uggla1.PNG', '../../css/pics/uggla1.PNG', '../../css/pics/uggla1.PNG', '../../css/pics/uggla1.PNG', '../../css/pics/uggla1.PNG', '../../css/pics/uggla1.PNG', '../../css/pics/uggla1.PNG', '../../css/pics/uggla1.PNG', '../../css/pics/uggla1.PNG', '../../css/pics/uggla1.PNG', '../../css/pics/uggla1.PNG', '../../css/pics/uggla1.PNG', '../../css/pics/uggla1.PNG' ]
+	
+	var counter = 0;
+	
+	setInterval (function() {changeimage()},100);
+	function changeimage(){var image = document.querySelector("#blink4");
+	image.setAttribute('src', bilder[counter]);
+	counter ++;
+	if (counter > 22){
+		counter = 0; }
+}// JavaScript Document
